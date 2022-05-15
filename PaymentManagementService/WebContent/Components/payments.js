926ded512e757c13babe935a27e8253c9168aa4d
Nisura Nisansith Weerasekara <88510451+Nisura-00@users.noreply.github.com>
2022-05-15 18:28:59 +0530

    Updatepayment.js

--- PaymentManagementService/WebContent/Components/payments.js
+++ PaymentManagementService/WebContent/Components/payments.js
@@ -1,60 +1,61 @@
 $(document).on("click", "#btnSave", function(event)
 { 
-// Clear alerts---------------------
- $("#alertSuccess").text(""); 
- $("#alertSuccess").hide(); 
- $("#alertError").text(""); 
- $("#alertError").hide(); 
-// Form validation-------------------
-var status = validatePaymentForm(); 
-if (status != true) 
- { 
- $("#alertError").text(status); 
- $("#alertError").show(); 
- return; 
- } 
-// If valid------------------------
-var type = ($("#hidPaymentIDSave").val() == "") ? "POST" : "PUT"; 
- $.ajax( 
- { 
- url : "PaymentAPI", 
- type : type, 
- data : $("#formPayment").serialize(), 
- dataType : "text", 
- complete : function(response, status) 
- { 
- onPaymentSaveComplete(response.responseText, status); 
- } 
- }); 
+         // Clear alerts---------------------
+          $("#alertSuccess").text(""); 
+          $("#alertSuccess").hide(); 
+          $("#alertError").text(""); 
+          $("#alertError").hide(); 
+		  
+         // Form validation-------------------
+         var status = validatePaymentForm(); 
+         if (status != true) 
+        { 
+          $("#alertError").text(status); 
+          $("#alertError").show(); 
+          return; 
+        } 
+        // If valid------------------------
+        var type = ($("#hidPaymentIDSave").val() == "") ? "POST" : "PUT"; 
+       $.ajax( 
+             { 
+              url : "PaymentAPI", 
+              type : type, 
+              data : $("#formPayment").serialize(), 
+              dataType : "text", 
+              complete : function(response, status) 
+             { 
+        onPaymentSaveComplete(response.responseText, status); 
+             } 
+         }); 
 });
 
-function onPaymentSaveComplete(response, status)
-{ 
-if (status == "success") 
- { 
- var resultSet = JSON.parse(response); 
- if (resultSet.status.trim() == "success") 
- { 
- $("#alertSuccess").text("Successfully saved."); 
- $("#alertSuccess").show(); 
- $("#divPaymentsGrid").html(resultSet.data); 
- } else if (resultSet.status.trim() == "error") 
- { 
- $("#alertError").text(resultSet.data); 
- $("#alertError").show(); 
- } 
- } else if (status == "error") 
- { 
- $("#alertError").text("Error while saving."); 
- $("#alertError").show(); 
- } else
- { 
- $("#alertError").text("Unknown error while saving.."); 
- $("#alertError").show(); 
+       function onPaymentSaveComplete(response, status)
+       { 
+         if (status == "success") 
+       { 
+        var resultSet = JSON.parse(response); 
+        if (resultSet.status.trim() == "success") 
+       { 
+           $("#alertSuccess").text("Successfully saved."); 
+           $("#alertSuccess").show(); 
+           $("#divPaymentsGrid").html(resultSet.data); 
+       } else if (resultSet.status.trim() == "error") 
+       { 
+           $("#alertError").text(resultSet.data); 
+           $("#alertError").show(); 
+       } 
+     } else if (status == "error") 
+    { 
+     $("#alertError").text("Error while saving."); 
+     $("#alertError").show(); 
+    } else
+    { 
+     $("#alertError").text("Unknown error while saving.."); 
+     $("#alertError").show(); 
+    }
+    $("#hidPaymentIDSave").val(""); 
+    $("#formPayment")[0].reset(); 
  }
-$("#hidPaymentIDSave").val(""); 
-$("#formPayment")[0].reset(); 
-}
 
 //UPDATE==========================================
   $(document).on("click", ".btnUpdate", function(event)
